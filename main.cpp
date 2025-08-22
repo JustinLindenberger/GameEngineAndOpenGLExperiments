43681375d1613b712216649cb484b17b04f8ed74
JustinLindenberger <106487613+JustinLindenberger@users.noreply.github.com>
2025-08-22 22:31:17 +0200

    Learned how to load textures, switch between them and use them at the same time. I wanted to create a rotating pyramid with a different texture on each side, but I've noticed that I can't do that so long as I haven't learned how to draw multiple 2D surfaces in a single draw call. If I draw them one after the other I would have to always draw the surface facing the camera last, because otherwise the back textures will be drawn over the front textures. I could implement this variable order of drawing surfaces but I suspect that the next two Learn OpenGL chapters contain automatic ways of doing this.

--- main.cpp
+++ main.cpp
@@ -5,24 +5,7 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include "Shader.h"
-
-const char* vertexShaderSource = "#version 460 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 460 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(ourColor, 1.0);\n"
-"}\0";
+#include "stb_image.h"
 
 void processInput(GLFWwindow* window)
 {
@@ -33,48 +16,6 @@ void processInput(GLFWwindow* window)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-}
-
-void initShaders(unsigned int shaderProgram)
-{
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER); //Creates a shader object
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //places the source code from vertexShaderSource inside of vertexShader
-    glCompileShader(vertexShader);
-
-    //Check if shader compilation was successful
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::LINKING_FAILED\n" << infoLog << std::endl;
-    }
 }
 
 int main()
@@ -103,67 +44,83 @@ int main()
 
     glViewport(0, 0, 800, 600);
 
-    int nrAttributes;
-    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-    std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+    Shader ourShader("vShader.txt", "fShader.txt");
+    ourShader.use();
 
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    initShaders(shaderProgram);
-    glUseProgram(shaderProgram);
+    // Configuration for how screen pixels interpolate between texture pixels
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    stbi_set_flip_vertically_on_load(true);
 
-    float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 0.0f,   // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 0.0f    // top 
+    float vert[4][15] =
+    {
+            // positions            //texture coords
+        {
+             0.5f, -0.5f, -0.5f,    0.55f, 0.95f, // bottom right left
+             0.5f, -0.5f,  0.5f,    0.55f, 0.35f, // bottom right right
+             0.0f,  0.5f,  0.0f,    0.85f, 0.65f  // top 
+        },
+        {
+             0.5f, -0.5f,  0.5f,    1.0f,  0.28f, // bottom right front 
+            -0.5f, -0.5f,  0.5f,    0.6f,  0.28f, // bottom left front
+             0.0f,  0.5f,  0.0f,    0.8f,  0.48f  // top 
+        },
+        {
+            -0.5f, -0.5f,  0.5f,    0.4f,  0.4f,  // bottom left left 
+            -0.5f, -0.5f, -0.5f,    0.0f,  0.25f, // bottom right left
+             0.0f,  0.5f,  0.0f,    0.0f,  0.8f   // top 
+        },
+        {
+             0.5f, -0.5f, -0.5f,    0.0f,  0.2f,  // bottom right back
+            -0.5f, -0.5f, -0.5f,    1.0f,  0.2f,  // bottom left back
+             0.0f,  0.5f,  0.0f,    0.5f,  0.9f   // top 
+        }
     };
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    unsigned int VBO;
-	glGenBuffers(1, &VBO);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
+    unsigned int VAO[4];
+    glGenVertexArrays(4, VAO);
+    unsigned int VBO[4];
+    glGenBuffers(4, VBO);
+    unsigned int texture[4];
+    glGenTextures(4, texture);
+    char const *textureFiles[4] = {"E.jpg", "L.jpg", "M.jpg", "T.jpg" };
+    int width[4], height[4], nrChannels[4];
+    unsigned char* data[4];
+    for (int i = 0; i < 4; i++)
+    {
+        glBindVertexArray(VAO[i]);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO[i]);
+        glBindTexture(GL_TEXTURE_2D, texture[i]);
+        data[i] = stbi_load(textureFiles[i], &width[i], &height[i], &nrChannels[i], 0);
+        if (data[i]) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width[i], height[i], 0, GL_RGB, GL_UNSIGNED_BYTE, data[i]);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else {
+            std::cout << "Failed to load texture number " << i << std::endl;
+        }
+        stbi_image_free(data[i]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vert[i]), vert[i], GL_DYNAMIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     while (!glfwWindowShouldClose(window))
     {
-		processInput(window);
-        glUseProgram(shaderProgram);
-
-        float timeValue = (fmod(glfwGetTime(), 3.0));
-        float decimal = timeValue - ((int)timeValue);
-        float negDecimal = 1 - decimal;
-        switch ((int)timeValue) {
-        case 0:
-            vertices[3] = negDecimal; vertices[4] = decimal; vertices[5] = 0.0f;
-            vertices[9] = decimal; vertices[10] = 0.0f; vertices[11] = negDecimal;
-            vertices[15] = 0.0f; vertices[16] = negDecimal; vertices[17] = decimal;
-            break;
-        case 1:
-            vertices[3] = 0.0f; vertices[4] = negDecimal; vertices[5] = decimal;
-            vertices[9] = negDecimal; vertices[10] = decimal; vertices[11] = 0.0f;
-            vertices[15] = decimal; vertices[16] = 0.0f; vertices[17] = negDecimal;
-            break;
-        case 2:
-            vertices[3] = decimal; vertices[4] = 0.0f; vertices[5] = negDecimal;
-            vertices[9] = 0.0f; vertices[10] = negDecimal; vertices[11] = decimal;
-            vertices[15] = negDecimal; vertices[16] = decimal; vertices[17] = 0.0f;
-            break;
-        default: 
-            break;
-        }
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-
+        processInput(window);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        for (int i = 0; i < 4; i++)
+        {
+            glBindTexture(GL_TEXTURE_2D, texture[i]);
+            glBindVertexArray(VAO[i]);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
