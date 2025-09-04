35c56afb2713d4648d3ce6266c54871a555eb3db
JustinLindenberger <106487613+JustinLindenberger@users.noreply.github.com>
2025-09-04 17:45:20 +0200

    Created a program to pick up and move cubes, tied to the movement of the camera.

---
+++ Cube.h
@@ -0,0 +1,125 @@
+#include "glm/glm.hpp"
+#include <glad/glad.h>
+#include "Shader.h"
+#include <iostream>
+#include <cmath>
+
+// Vertices for the triangles that compose the cube, each block of 6 corresponds to one face.
+const float vertices[] = {
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f
+};
+
+class Cube
+{
+private:
+    unsigned int VBO, VAO;
+    // Shader that will be applied to this cube, we need to know the shader class so that we can modify its uniform variables.
+    Shader* shader;
+
+public:
+    glm::vec3 Position;
+    // Is the player looking at this cube? true if yes (cube colored red), false if no (cube colored white).
+    bool targeted = false;
+
+    // This checks whether the line of sight (the line of the front vector) of the player intersects with any cube faces.
+    bool isCubeTargeted(glm::vec3 cameraPos, glm::vec3 cameraFront) {
+        for (int axis = 0; axis < 3; ++axis) {
+            float dir = cameraFront[axis];
+            if (dir == 0) continue;                         // Front is prallel in to this plane, skip to prevent Divide by Zero.
+
+            float faceOffset = (dir > 0.0f) ? -0.5f : 0.5f; // Check whether nearer or farther cube face is closer to camera so we only check one plane.
+            float planePos = Position[axis] + faceOffset;
+            float t = (planePos - cameraPos[axis]) / dir;   // Determine scalar at which the front vector intersects the cube plane.
+
+            if (t < 0.0f) continue;                         // Scalar is negative, so cube is behind camera.
+
+            // Compute intersection point coordinates on the other two axes.
+            int a1 = (axis + 1) % 3;
+            int a2 = (axis + 2) % 3;
+            float interA1 = cameraPos[a1] + t * cameraFront[a1];
+            float interA2 = cameraPos[a2] + t * cameraFront[a2];
+
+            // Check whether the intersections are within the cube face.
+            if (interA1 > (Position[a1] - 0.5f) && interA1 < (Position[a1] + 0.5) &&
+                interA2 > (Position[a2] - 0.5f) && interA2 < (Position[a2] + 0.5)) {
+                targeted = true;
+                return true;
+            }
+        }
+        targeted = false;
+        return false;
+    }
+
+    // Upon construction load all the vertex data into a buffer for quick retrival later on.
+    Cube(float x, float y, float z, Shader& sha) : shader(&sha) {
+        Position = glm::vec3(x, y, z);
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+    }
+
+    void drawCube() {
+        // Load the buffer containing the cube vertex data.
+        glBindVertexArray(VAO);
+
+        // This matrix translates the cubes vertex coordinates to its actual position.
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), Position);
+        shader->setMatrix4fv("model", model);
+
+        // Sets the "targeted" uniform bool variable in the shader program, which changes to color to red.
+        if (targeted) {
+            shader->setBool("targeted", true);
+        } else {
+            shader->setBool("targeted", false);
+        }
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+};
+
--- main.cpp
+++ main.cpp
@@ -2,26 +2,27 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <algorithm>
-#define _USE_MATH_DEFINES
-#include <cmath>
 #include "Shader.h"
 #include "Camera.h"
-#include "stb_image.h"
+#include "Cube.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+#include <chrono>
+#include <thread>
+#include <vector>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 
-// settings
+// screen settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 1.5f, 4.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -30,6 +31,13 @@ bool firstMouse = true;
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
+// Cube that the camera is currently looking at (nullptr if no cube is looked at)
+Cube* targetedCube = nullptr;
+
+// Comparision function to sort cubes by distance to the camera, smallest distance first
+bool sortCubes(Cube a, Cube b) {
+    return (glm::distance(camera.Position, a.Position) < glm::distance(camera.Position, b.Position));
+};
 
 int main()
 {
@@ -47,8 +55,10 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);glfwSetScrollCallback(window, scroll_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, GLFW_TRUE);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -60,149 +70,61 @@ int main()
     Shader ourShader("vShader.txt", "fShader.txt");
     ourShader.use();
 
-    // Configuration for how screen pixels interpolate between texture pixels
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    stbi_set_flip_vertically_on_load(true);
+    // Initally places 9 cubes in a 3x3 grid.
+    Cube cube0(0.0f, 0.0f, 0.0f, ourShader);
+    Cube cube1(1.5f, 0.0f, 1.5f, ourShader);
+    Cube cube2(1.5f, 0.0f, 0.0f, ourShader);
+    Cube cube3(1.5f, 0.0f, -1.5f, ourShader);
+    Cube cube4(0.0f, 0.0f, -1.5f, ourShader);
+    Cube cube5(-1.5f, 0.0f, -1.5f, ourShader);
+    Cube cube6(-1.5f, 0.0f, 0.0f, ourShader);
+    Cube cube7(-1.5f, 0.0f, 1.5f, ourShader);
+    Cube cube8(0.0f, 0.0f, 1.5f, ourShader);
 
-    float vert[4][15] =
-    {
-            // positions            //texture coords
-        {
-             0.5f, -0.5f, -0.5f,    0.85f, 0.45f, // bottom right left
-             0.5f, -0.5f,  0.5f,    0.25f, 0.45f, // bottom right right
-             0.0f,  0.5f,  0.0f,    0.55f, 0.90f  // top 
-        },
-        {
-             0.5f, -0.5f,  0.5f,    1.0f,  0.28f, // bottom right front 
-            -0.5f, -0.5f,  0.5f,    0.6f,  0.28f, // bottom left front
-             0.0f,  0.5f,  0.0f,    0.8f,  0.48f  // top 
-        },
-        {
-            -0.5f, -0.5f,  0.5f,    0.4f,  0.4f,  // bottom left left 
-            -0.5f, -0.5f, -0.5f,    0.0f,  0.25f, // bottom right left
-             0.0f,  0.5f,  0.0f,    0.0f,  0.8f   // top 
-        },
-        {
-             0.5f, -0.5f, -0.5f,    0.0f,  0.2f,  // bottom right back
-            -0.5f, -0.5f, -0.5f,    1.0f,  0.2f,  // bottom left back
-             0.0f,  0.5f,  0.0f,    0.5f,  0.9f   // top 
-        }
-    };
-    float valentexe[] = {
-        -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, //links oben
-        -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, //links unten
-        0.5f, 1.0f, 0.0f, 1.0f, 1.0f, //rechts oben
-        0.5f, -1.0f, 0.0f, 1.0f, 0.0f //rechts unten
-    };
-    unsigned int indices[] = {
-        0, 1, 2,
-        1, 2, 3
-    };
-
-    unsigned int EBO, VVAO, VVBO, Vtexture;
-    glGenBuffers(1, &EBO);
-    glGenBuffers(1, &VVBO);
-    glGenVertexArrays(1, &VVAO);
-    glGenTextures(1, &Vtexture);
-    int Vwidth, Vheight, VnrChannels;
-    unsigned char* Vdata;
-
-    glBindVertexArray(VVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VVBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBindTexture(GL_TEXTURE_2D, Vtexture);
-    
-    Vdata = stbi_load("V.jpg", &Vwidth, &Vheight, &VnrChannels, 0);
-    if (Vdata) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, Vwidth, Vheight, 0, GL_RGB, GL_UNSIGNED_BYTE, Vdata);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(Vdata);
-    
-    glBufferData(GL_ARRAY_BUFFER, sizeof(valentexe), valentexe, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    unsigned int VAO[4];
-    glGenVertexArrays(4, VAO);
-    unsigned int VBO[4];
-    glGenBuffers(4, VBO);
-    unsigned int texture[4];
-    glGenTextures(4, texture);
-    char const *textureFiles[4] = {"E.jpg", "L.jpg", "M.jpg", "T.jpg" };
-    int width[4], height[4], nrChannels[4];
-    unsigned char* data[4];
-
-    for (int i = 0; i < 4; i++)
-    {
-        glBindVertexArray(VAO[i]);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO[i]);
-        glBindTexture(GL_TEXTURE_2D, texture[i]);
-        
-        data[i] = stbi_load(textureFiles[i], &width[i], &height[i], &nrChannels[i], 0);
-        if (data[i]) {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width[i], height[i], 0, GL_RGB, GL_UNSIGNED_BYTE, data[i]);
-            glGenerateMipmap(GL_TEXTURE_2D);
-        }
-        else {
-            std::cout << "Failed to load texture number " << i << std::endl;
-        }
-        stbi_image_free(data[i]);
-        
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vert[i]), vert[i], GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-        glEnableVertexAttribArray(1);
-    }
-
-    glm::mat4 model;
-    glm::mat4 proj;
+    // Matrix that projects all geometry to normalized device coordinates, which are required by openGL to draw shapes.
+    glm::mat4 proj = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    ourShader.setMatrix4fv("projection", proj);
     glm::mat4 view;
+
+    std::vector<Cube> cubes = { cube0, cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8 };
+
+    /* This loop first calculates the time passed between frames (needed to scale camera movement), 
+    * sorts cubes by distance to the camera, checks in order whether the camera is looking at (targeting)
+    * a cube. If the left mouse button is held the cube will be tied to the camera movement and move 
+    * and turn with the camera. All cubes are drawn, targeted cube is red, all other cubes are white.
+    */
     while (!glfwWindowShouldClose(window))
     {
-        processInput(window);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-
+        
+        // Retrieve the matrix that enforces the cameras viewing angle of the game world.
         view = camera.GetViewMatrix();
         ourShader.setMatrix4fv("view", view);
-        proj = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        ourShader.setMatrix4fv("projection", proj);
 
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.5));
-        model = glm::scale(model, glm::vec3(2.0f, 1.0f, 3.0f));
-        ourShader.setMatrix4fv("model", model);
+        std::sort(cubes.begin(), cubes.end(), sortCubes);
 
-        glBindTexture(GL_TEXTURE_2D, Vtexture);
-        glBindVertexArray(VVAO);
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        for (int i = 0; i < 4; i++)
-        {    
-            model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-            ourShader.setMatrix4fv("model", model);
-
-            glBindTexture(GL_TEXTURE_2D, texture[i]);
-            glBindVertexArray(VAO[i]);
-
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+        // Targeted flag also determins cube color, so it needs to be reset so that cubes aren't all painted red over time. 
+        for (int i = 0; i < cubes.size(); i++) {
+            cubes[i].targeted = false;
         }
+        targetedCube = nullptr;
+        for (int i = 0; i < cubes.size(); i++) {
+            if (cubes[i].isCubeTargeted(camera.Position, camera.Front)) {
+                targetedCube = &cubes[i];
+                break;
+            }
+        }
+        for (int i = 0; i < cubes.size(); i++) {
+            cubes[i].drawCube();
+        }
+
+        processInput(window);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -212,11 +134,14 @@ int main()
     return 0;
 }
 
+// A, W, S, and D and are used to steer the camera Left, Forward, Backwards and Right.
 void processInput(GLFWwindow* window)
 {
+    // ESC stops the rendering loop and terminates the program.
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
+    glm::vec3 previousPos = camera.Position;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -225,8 +150,15 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    // If the mouse button is held the cube is displayed by the same movement as the camera.
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        if (targetedCube)
+            targetedCube->Position += (camera.Position - previousPos);
+    }
 }
 
+// Rescales the viewport when the window is resized.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -245,14 +177,40 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
     }
 
     float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; 
+    float yoffset = lastY - ypos;
 
     lastX = xpos;
     lastY = ypos;
 
+    // Save previous orientation so that we can calculate the change in direction to the previus frame.
+    float prevYaw = camera.Yaw;
+    float prevPitch = camera.Pitch;
+    // Pass on the mouse movement offsets which are then translated into camera rotations.
     camera.ProcessMouseMovement(xoffset, yoffset);
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        if (targetedCube) {
+            // acquire vector pointing from camera to targeted cube
+            glm::vec3 cameraToCube = targetedCube->Position - camera.Position;
+            
+            float dYaw = glm::radians(camera.Yaw - prevYaw);
+            float dPitch = glm::radians(camera.Pitch - prevPitch);
+
+            // Rotate cameraToCube vector by change in player direction.
+            // dYaw is negated because a camera rotation the right results in a positive dYaw
+            // but a positive dYaw means a rotation to the left. Rotating the camera downards 
+            // makes dPitch positive, and positive dPatch also corresponds to a downwards rotation.
+            glm::mat4 RAroundY = glm::rotate(glm::mat4(1.0f), -dYaw, glm::vec3(0, 1, 0));
+            glm::mat4 RAroundRight = glm::rotate(glm::mat4(1.0f), dPitch, camera.Right);
+            glm::mat4 R = RAroundRight * RAroundY;
+            glm::vec3 rotated = glm::mat3(R) * cameraToCube;
+
+            targetedCube->Position = camera.Position + rotated;
+        }
+    }
 }
 
+// Scrolling the mouse changes the FOV.
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
